781d78fae0abe191e16f69d04f35cca61e1c35a9
Ozeliurs <ozeliurs@gmail.com>
2023-04-07 15:14:36 +0200

    feat: lexical analysis
    Co-authored-by: loicpalayer <84199585+loicpalayer@users.noreply.github.com>

--- analyse_lexicale.py
+++ analyse_lexicale.py
@@ -1,51 +1,74 @@
 import sys
+
 from sly import Lexer
 
+
 class FloLexer(Lexer):
-	# Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
-	tokens = { IDENTIFIANT, ENTIER, ECRIRE,INFERIEUR_OU_EGAL }
+    # Noms des lexèmes (sauf les litéraux). En majuscule. Ordre non important
+    tokens = {
+        TYPE, IDENTIFIANT, ENTIER, ECRIRE, LIRE, ET, OU, NON, SI, SINONSI,
+        SINON, TANTQUE, RETOURNER, EGAL, DIFFERENT, INFERIEUR_OU_EGAL,
+        SUPERIEUR_OU_EGAL, INFERIEUR, SUPERIEUR
+    }
 
-	#Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale. 
-	#Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
-	#Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
-	literals = { '+','*','(',')',";" }
-	
-	# chaines contenant les caractère à ignorer. Ici espace et tabulation
-	ignore = ' \t'
+    # Les caractères litéraux sont des caractères uniques qui sont retournés tel quel quand rencontré par l'analyse lexicale.
+    # Les litéraux sont vérifiés en dernier, après toutes les autres règles définies par des expressions régulières.
+    # Donc, si une règle commence par un de ces littérals (comme INFERIEUR_OU_EGAL), cette règle aura la priorité.
+    literals = {'+', '-', '*', '/', '%', '(', ')', ';', '{', '}', ',', '='}
 
-	# Expressions régulières correspondant au différents Lexèmes par ordre de priorité
-	INFERIEUR_OU_EGAL= r'<='
-	
-	@_(r'0|[1-9][0-9]*')
-	def ENTIER(self, t):
-		t.value = int(t.value)
-		return t
+    # chaines contenant les caractère à ignorer. Ici espace et tabulation
+    ignore = ' \t'
 
-    	# cas général
-	IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*' #en général, variable ou nom de fonction
+    # cas général
+    IDENTIFIANT = r'[a-zA-Z][a-zA-Z0-9_]*'  # en général, variable ou nom de fonction
 
-	# cas spéciaux:
-	IDENTIFIANT['ecrire'] = ECRIRE
-	
-	#Syntaxe des commentaires à ignorer
-	ignore_comment = r'\#.*'
+    # cas spéciaux:
+    IDENTIFIANT['entier'] = TYPE
+    IDENTIFIANT['booleen'] = TYPE
+    IDENTIFIANT['ecrire'] = ECRIRE
+    IDENTIFIANT['lire'] = LIRE
+    IDENTIFIANT['et'] = ET
+    IDENTIFIANT['ou'] = OU
+    IDENTIFIANT['non'] = NON
+    IDENTIFIANT['si'] = SI
+    IDENTIFIANT['sinonsi'] = SINONSI
+    IDENTIFIANT['sinon'] = SINON
+    IDENTIFIANT['tantque'] = TANTQUE
+    IDENTIFIANT['retourner'] = RETOURNER
 
-	# Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
-	@_(r'\n+')
-	def ignore_newline(self, t):
-		self.lineno += t.value.count('\n')
+    # Expressions régulières correspondant au différents Lexèmes par ordre de priorité
+    EGAL = r'=='
+    DIFFERENT = r'!='
+    INFERIEUR_OU_EGAL = r'<='
+    SUPERIEUR_OU_EGAL = r'>='
+    INFERIEUR = r'<'
+    SUPERIEUR = r'>'
 
-	# En cas d'erreur, indique où elle se trouve
-	def error(self, t):
-		print(f'Ligne{self.lineno}: caractère inattendu "{t.value[0]}"')
-		self.index += 1
+    @_(r'0|[1-9][0-9]*')
+    def ENTIER(self, t):
+        t.value = int(t.value)
+        return t
+
+    # Syntaxe des commentaires à ignorer
+    ignore_comment = r'\#.*'
+
+    # Permet de conserver les numéros de ligne. Utile pour les messages d'erreurs
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    # En cas d'erreur, indique où elle se trouve
+    def error(self, t):
+        print(f'Ligne{self.lineno}: caractère inattendu "{t.value[0]}"')
+        self.index += 1
+
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
-	else:
-		with open(sys.argv[1],"r") as f:
-			data = f.read()
-			lexer = FloLexer()
-			for tok in lexer.tokenize(data):
-				print(tok)
+    if len(sys.argv) < 2:
+        print("usage: python3 analyse_lexicale.py NOM_FICHIER_SOURCE.flo")
+    else:
+        with open(sys.argv[1], "r") as f:
+            data = f.read()
+            lexer = FloLexer()
+            for tok in lexer.tokenize(data):
+                print(tok)
