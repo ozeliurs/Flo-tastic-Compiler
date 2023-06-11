faa64bd3307fc9e6044575749404a3903910550e
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-11 18:52:37 +0200

    46/48 : fix  python test paths

--- tests/test_analyse_syntaxique.py
+++ tests/test_analyse_syntaxique.py
@@ -14,7 +14,7 @@ class TestFloParser(TestCase):
     lexer: Lexer = FloLexer()
     parser: Parser = FloParser()
     TEST_FILES = [
-        DATA_DIR / "input/fonction_2.flo",
+        DATA_DIR / "input/fonction_3.flo",
     ]
 
     TEST_FILES_BAD = (DATA_DIR / "bad_input").iterdir()
@@ -23,18 +23,11 @@ class TestFloParser(TestCase):
     def before_each(self):
         self.lexer = FloLexer()
 
-    def test_integer(self):
-        tokens = self.lexer.tokenize('1')
-        tree = self.parser.parse(tokens)
-        tree.afficher()
-        self.assertTrue(tree is not None)
-
     def test_files(self):
         for file in self.TEST_FILES:
             with open(file, "r", encoding="utf8") as f:
                 data = f.read()
-                tokens = self.lexer.tokenize(data)
-                tree = self.parser.parse(tokens)
+                tree = analyse_syntaxique.parse(data)
                 tree.afficher()
                 print(tree.liste_instructions.get_variable_definitions())
                 gen_programme(tree)
