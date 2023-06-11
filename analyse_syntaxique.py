3685862bd3ce9e6abf9654541673da4ebfe732cc
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-11 18:54:29 +0200

    46/48 : fix  python test paths

--- analyse_syntaxique.py
+++ analyse_syntaxique.py
@@ -241,7 +241,7 @@ if __name__ == '__main__':
         with open(sys.argv[1], "r") as f:
             data = f.read()
             try:
-                arbre = parser.parse(lexer.tokenize(data))
+                arbre = parse(data)
                 arbre.afficher()
             except EOFError:
                 exit()
